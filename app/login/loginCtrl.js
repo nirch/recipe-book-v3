71883290d2ee998bb2414f687809ad2219bee022
nirch <nirh2@yahoo.com>
2018-12-02 11:43:46 +0200

    added login function to ctrl (fake) move to recipes page

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,4 +1,17 @@
 
-app.controller("loginCtrl", function($scope) {
-    $scope.test = "blblblbl";
+app.controller("loginCtrl", function($scope, $location) {
+    
+    $scope.invalidLogin = false;
+
+    $scope.login = function() {
+        $scope.invalidLogin = false;
+        
+        if ($scope.email === "nir@nir.com" && $scope.pwd === "123") {
+            // success login
+            $location.path("/recipes")
+        } else {
+            // failed login
+            $scope.invalidLogin = true;
+        }
+    }
 });
