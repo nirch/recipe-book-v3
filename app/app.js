0ff06befb8f821a9315bd455979f1735ad6ef3d6
nirch <nirh2@yahoo.com>
2018-12-02 10:13:09 +0200

    Adding routing + home.html (empty)

--- app/app.js
+++ app/app.js
@@ -1,2 +1,19 @@
 
-var app = angular.module("recipeBookApp", []);
+var app = angular.module("recipeBookApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+
+    }).when("/signup", {
+
+    }).when("/recipes", {
+
+    }).when("/new" , {
+
+    }).when("/recipe/:id" , {
+
+    })
+})
