2bcd6913fd065b9a503f251645025f2ce8709dd8
nirch <nirh2@yahoo.com>
2018-12-03 09:36:11 +0200

    hack for getActiveUserRecipes to call server only once

--- app/model/recipesSrv.js
+++ app/model/recipesSrv.js
@@ -2,6 +2,7 @@
 app.factory("recipes", function($q, $http, user) {
 
     var recipes = [];
+    var wasEverLoaded = false;
 
     function Recipe(plainRecipe) {
         this.id = plainRecipe.id;
@@ -13,31 +14,31 @@ app.factory("recipes", function($q, $http, user) {
         this.userId = plainRecipe.userId;
     }
 
-
-
-
     function getActiveUserRecipes() {
         var async = $q.defer();
 
-        recipes = [];
-        
-        var getRecipesURL = "http://my-json-server.typicode.com/nirch/recipe-book-v3/recipes?userId=" +
-            user.getActiveUser().id;
-        
-        $http.get(getRecipesURL).then(function(response) {
-            for (var i = 0; i < response.data.length; i++) {
-                var recipe = new Recipe(response.data[i]);
-                recipes.push(recipe);
-            }            
+        // This is a hack since we don't really have a persistant server.
+        // So I want to get all recipes only once.
+        if (wasEverLoaded) {
             async.resolve(recipes);
-        }, function(error) {
-            async.reject(error);
-        })
-
+        } else {
+            var getRecipesURL = "http://my-json-server.typicode.com/nirch/recipe-book-v3/recipes?userId=" +
+                user.getActiveUser().id;
+            
+            $http.get(getRecipesURL).then(function(response) {
+                for (var i = 0; i < response.data.length; i++) {
+                    var recipe = new Recipe(response.data[i]);
+                    recipes.push(recipe);
+                }
+                wasEverLoaded = true;
+                async.resolve(recipes);
+            }, function(error) {
+                async.reject(error);
+            });
+        }
 
         return async.promise;
     }
-
 
 
     function createRecipe(name, description, ingredients, steps, imgUrl) {
