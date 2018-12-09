252f76a0f23dc8512b9a7865a79ea034376ba786
nirch <nirh2@yahoo.com>
2018-12-09 10:02:34 +0200

    using image upload directive

--- app/recipes/newRecipeCtrl.js
+++ app/recipes/newRecipeCtrl.js
@@ -10,7 +10,7 @@ app.controller("newRecipeCtrl", function($scope, recipes, $location, user) {
 
     $scope.createRecipe = function () {
         recipes.createRecipe($scope.name, $scope.description, 
-            $scope.ingrediants, $scope.steps,  $scope.image).then(function () {
+            $scope.ingrediants, $scope.steps,  $scope.image.src).then(function () {
             $location.path("/recipes")
         }, function (err) {
             console.log(err);
